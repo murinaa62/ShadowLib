6dd65e01f3e5b0856260d814508c3aa0f7b3e355
renlov <a.pimenov95@mail.ru>
2022-05-20 12:06:48 +0300

    added

---
+++ shadow/cpp.cpp
@@ -0,0 +1,5 @@
+//
+// Created by renlo on 20.05.2022.
+//
+
+#include "cpp.h"
---
+++ shadow/cpp.h
@@ -0,0 +1,14 @@
+//
+// Created by renlo on 20.05.2022.
+//
+
+#ifndef SHADOW_CPP_H
+#define SHADOW_CPP_H
+
+
+class cpp {
+
+};
+
+
+#endif //SHADOW_CPP_H
